9751233705db661f16ce2aeed5909fba3d4bbe0e
shreyash3087 <shreyash3087@gmail.com>
2024-06-16 00:08:07 +0530

    Added Social Media Icons

--- src/components/AdminCard/AdmCard.jsx
+++ src/components/AdminCard/AdmCard.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 const AdmCard = ({ admin, row }) => {
   return (
     <>
@@ -7,13 +9,23 @@ const AdmCard = ({ admin, row }) => {
         {/* Card for larger screens */}
         <div className={`hidden md:flex h-96 mb-5 flex-row ${row}`}>
           <div className="w-1/4 md:w-1/6 bg-[#15281f] post border flex justify-center items-center text-2xl md:text-3xl lg:text-4xl font-light">
-            <div className='-rotate-90'>{admin.role}</div>
+            <div className="-rotate-90">{admin.role}</div>
           </div>
           <div className="w-1/2 md:w-3/6 p-6 relative border text-color flex justify-start items-center max-xl:flex-col bg-[#eafbea]">
-            <img src={admin.image} className="w-32 md:w-72 h-32 md:h-72 rounded-xl" alt="Logo" />
-            <h1 className="text-5xl max-xl:text-3xl font-extrabold px-5 w-full ">
-              <span className="text-[#254336]">{admin.name}</span>
-            </h1>
+            <img
+              src={admin.image}
+              className="max-md:w-32 max-md:h-32 max-xl:w-60 max-xl:h-60 w-72 h-72 rounded-xl"
+              alt="Logo"
+            />
+            <div>
+              <h1 className="text-5xl max-xl:text-3xl font-extrabold px-5 w-full ">
+                <span className="text-[#254336]">{admin.name}</span>
+              </h1>
+              <div className="w-full flex justify-center mt-2 gap-4 cursor-pointer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </div>
+            </div>
           </div>
           <div className="w-1/4 md:w-2/6 border text-5xl md:text-6xl lg:text-7xl font-bold flex justify-center items-center post fade-green-bg">
             <span>{admin.unicode}</span>
@@ -26,18 +38,21 @@ const AdmCard = ({ admin, row }) => {
             {admin.role}
           </div>
           <div className="w-full p-4 relative border text-color flex flex-col justify-center items-center bg-[#eafbea]">
-            <img src={admin.image} className="border w-48 h-48 mb-4" alt="Logo" />
-            <h1 className="text-3xl font-extrabold pb-2">
-              {admin.name}
-            </h1>
+            <img
+              src={admin.image}
+              className="border w-48 h-48 mb-4"
+              alt="Logo"
+            />
+            <h1 className="text-3xl font-extrabold pb-2">{admin.name}</h1>
+            <div className="w-full flex justify-center mt-2 gap-4 cursor-pointer">
+            <FontAwesomeIcon icon={faLinkedin} style={{ width: '25px', height: '25px' }} />
+            <FontAwesomeIcon icon={faInstagram} style={{ width: '25px', height: '25px' }} />
           </div>
-
+          </div>
         </div>
       </section>
-
-
     </>
-  )
-}
+  );
+};
 
-export default AdmCard
+export default AdmCard;
--- src/components/Member-Card/MemberCard.jsx
+++ src/components/Member-Card/MemberCard.jsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
-import { useScroll } from 'framer-motion';
-import { motion } from 'framer-motion';
-
+import React, { useRef } from "react";
+import { useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 const MemberCard = ({ admin, bgcolor, row, arrow }) => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -16,45 +18,61 @@ const MemberCard = ({ admin, bgcolor, row, arrow }) => {
       <section
         className={`hidden md:flex flex-row h-96 mb-10 p-5 items-center justify-around ${bgcolor} ${row}`}
       >
-        <div className='relative w-72 h-72 '>
-        <img src={admin.image} className='absolute object-cover object-center h-full rounded-xl' alt='Admin' />
+        <div className="relative w-72 h-72 ">
+          <img
+            src={admin.image}
+            className="absolute object-cover object-center h-full rounded-xl"
+            alt="Admin"
+          />
         </div>
         <motion.div
           ref={ref}
           style={{
             scale: scrollYProgress,
             opacity: scrollYProgress,
-            transition: 'transform 0.3s ease-in-out, opacity 0.3s ease-in-out',
+            transition: "transform 0.3s ease-in-out, opacity 0.3s ease-in-out",
           }}
-          className='h-[80%] p-5 w-[40%] glass-effect flex flex-col items-center justify-center bg-[#96BE96] bg-opacity-20 rounded-lg'
+          className="h-[80%] p-5 w-[40%] glass-effect flex flex-col items-center justify-center bg-[#96BE96] bg-opacity-20 rounded-lg"
         >
-          <h1 className='post text-3xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-extrabold leading-none'>
+          <h1 className="post text-3xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-extrabold leading-none">
             {admin.name}
           </h1>
           <br />
-          <p className='post leading-none font-bold'>({admin.role})</p>
+          <p className="post leading-none font-bold">({admin.role})</p>
+          <div className="w-full flex justify-center mt-4 gap-4 cursor-pointer">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </div>
         </motion.div>
       </section>
-      
+
       {/* Smaller devices */}
       <section
         className={`flex flex-col md:hidden h-auto mb-10 p-5 gap-5 items-center justify-around ${bgcolor} ${row}`}
       >
-        <img src={admin.image} className='w-48 h-48 sm:w-36 sm:h-36 rounded-lg' alt='Admin' />
+        <img
+          src={admin.image}
+          className="w-48 h-48 sm:w-36 sm:h-36 rounded-lg"
+          alt="Admin"
+        />
         <motion.div
           ref={ref}
           style={{
             scale: scrollYProgress,
             opacity: scrollYProgress,
-            transition: 'transform 0.3s ease-in-out, opacity 0.3s ease-in-out',
+            transition: "transform 0.3s ease-in-out, opacity 0.3s ease-in-out",
           }}
-          className='h-36 w-72 glass-effect flex flex-col items-center justify-center bg-[#96BE96] bg-opacity-20 rounded-lg'
+          className="h-36 w-72 glass-effect flex flex-col items-center justify-center bg-[#96BE96] bg-opacity-20 rounded-lg"
         >
-          <h1 className='post text-2xl font-extrabold leading-none'>
+          <h1 className="post text-2xl font-extrabold leading-none">
             {admin.name}
           </h1>
           <br />
-          <p className='post leading-none font-lg'>({admin.role})</p>
+          <p className="post leading-none font-lg">({admin.role})</p>
+          <div className="w-full flex justify-center mt-4 gap-4 cursor-pointer">
+            <FontAwesomeIcon icon={faLinkedin} style={{ width: '25px', height: '25px' }} />
+            <FontAwesomeIcon icon={faInstagram} style={{ width: '25px', height: '25px' }} />
+          </div>
         </motion.div>
       </section>
     </>
