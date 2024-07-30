1d071a95fa43874f5a7d7990a4249f4d0b840cd5
HARSHITA JAISWAL <140200820+HarshitaJaiswal5@users.noreply.github.com>
2024-07-30 13:12:28 -0700

    Update constants.jsx
    deleted unnecessary constants

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -5,13 +5,6 @@ import { ShieldHalf } from "lucide-react";
 import { PlugZap } from "lucide-react";
 import { GlobeLock } from "lucide-react";
 
-// import user1 from "../assets/profile-pictures/Giriraj.jpg";
-// import user2 from "../";
-import user3 from "../assets/profile-pictures/user3.jpg";
-import user4 from "../assets/profile-pictures/user4.jpg";
-import user5 from "../assets/profile-pictures/user5.jpg";
-import user6 from "../assets/profile-pictures/user6.jpg";
-
 export const navItems = [
   { label: "Events", href: "/events" },
   { label: "About", href: "/about" },
